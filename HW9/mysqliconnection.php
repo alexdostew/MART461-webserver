be9e9667f76923d2195caaa4fd188e9b889ab8ab
alexdostew <Alexdostew@outlook.com>
2021-11-01 19:02:39 -0600

    update

--- HW9/mysqliconnection.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servername = "localhost"; // this is the server name
-$usn = "ads95";
-$password = "-f_95LL^x";
-$dbname = "mart461";
-
-// Create connection
-$conn = new mysqli($servername, $usn, $password, $dbname);
-?>
